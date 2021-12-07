e2de8eccb3060eefae5c51d7fd55eee5b2124c6e
Emilio <Edwardestevez95@gmail.com>
2021-12-06 22:32:21 -0600

    pushing final

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,7 +8,7 @@ export default function About() {
       <h1>About Page</h1>
 <div className="align">
 <h2> Hello, I am Emilio Estevez and I am a driven, dedicated,
-    and detail oriented Web Developer in Austin, TX. This was all done in React.js! Here is my <a href="https://www.linkedin.com/in/edward-estevez-962789175/" target="_blank">LinkedIn! </a> and <a href={resume} target="_blank">Resume! </a> 
+    and detail oriented Web Developer in Austin, TX. This was all done in React.js! Here is my <a href="https://www.linkedin.com/in/edward-estevez-962789175/" target="_blank">LinkedIn! </a> , <a href={resume} target="_blank">Resume! </a> and <a href="https://github.com/Emilio512" target="_blank">Github! </a> 
 </h2>
 
 </div>
